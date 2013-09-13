9d76b9f841f92bb9f7ef33e57a48d2afec8e9fc1
Nicholas <nicholas.lee.3@gmail.com>
2013-09-13 16:34:12 -0500

    .

--- config/environment.rb
+++ config/environment.rb
@@ -24,8 +24,8 @@ require 'erb'
 require 'debugger'
 
 # Some helper constants for path-centric logic
+set :root, File.dirname(__FILE__)
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-
 APP_NAME = APP_ROOT.basename.to_s
 
 # Set up the controllers and helpers
